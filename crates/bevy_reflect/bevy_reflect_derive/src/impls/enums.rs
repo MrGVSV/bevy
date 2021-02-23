f984a026de341aa1a72c428b69d22d82dbcf8bc9
Brice DAVIER <bricedavier@gmail.com>
2021-02-23 11:14:14 +0900

    Fix wrappers using wrong ident

--- crates/bevy_reflect/bevy_reflect_derive/src/impls/enums.rs
+++ crates/bevy_reflect/bevy_reflect_derive/src/impls/enums.rs
@@ -316,7 +316,7 @@ pub(crate) fn impl_enum(
         };);
         token_stream.extend(TokenStream::from(quote! {
             #[repr(transparent)]
-            pub struct #wrapper_ident(TestEnum);
+            pub struct #wrapper_ident(enum_name);
             impl #bevy_reflect_path::Reflect for #wrapper_ident {
                 fn type_name(&self) -> &str {
                     #wrapper_name
@@ -458,7 +458,7 @@ pub(crate) fn impl_enum(
         };);
         token_stream.extend(TokenStream::from(quote! {
             #[repr(transparent)]
-            pub struct #wrapper_ident(TestEnum);
+            pub struct #wrapper_ident(enum_name);
             impl #bevy_reflect_path::Reflect for #wrapper_ident {
                 fn type_name(&self) -> &str {
                     #wrapper_name
